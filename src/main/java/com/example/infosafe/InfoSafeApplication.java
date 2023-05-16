1649e441df2a41c898ebd9806c230120227f9dc4
Chris Mittendorf <u15092462@tuks.co.za>
2023-05-16 11:11:32 +0200

    Project Creation
    Initial Project Creation

---
+++ src/main/java/com/example/infosafe/InfoSafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.infosafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoSafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfoSafeApplication.class, args);
+
+    }
+}
---
+++ src/test/java/com/example/infosafe/InfoSafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infosafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoSafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
