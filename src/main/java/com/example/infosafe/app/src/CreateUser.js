47a321ddf50c49dedc245b7cd8326cfa6b0cd5f7
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-05-20 23:55:43 +0200

    Created the User Creation page.

---
+++ src/main/java/com/example/infosafe/app/src/CreateUser.js
@@ -0,0 +1,24 @@
+import './CreateUser.css'
+
+function CreateUser() {
+    return (
+      <div className="backgrnd">
+          <div className="overlay">
+              <p className="page_name">User Creation</p>
+
+              <p className="label_name">Name</p>
+              <input className="name" name="name"/>
+              <p className="label_surname">Surname</p>
+              <input className="surname" name="surname"/>
+              <p className="label_email">Email</p>
+              <input className="email" name="email"/>
+              <p className="label_password">Password</p>
+              <input className="password" name="password"/>
+
+
+          </div>
+      </div>
+
+    )
+}
+export default  CreateUser;
