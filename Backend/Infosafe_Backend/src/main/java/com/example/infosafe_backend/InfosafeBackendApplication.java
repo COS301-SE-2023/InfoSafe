257ccdf135832e615b5420cb889bcf9f373e3632
Chris Mittendorf <u15092462@tuks.co.za>
2023-06-09 13:44:47 +0200

    New Project Created
    New project created in backend folder

---
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/InfosafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.infosafe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosafeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfosafeBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/Infosafe_Backend/src/test/java/com/example/infosafe_backend/InfosafeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infosafe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfosafeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
