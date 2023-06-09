38c0b76650f48b6f959f21e3743b93ac14a2d698
AlistairMRoss <alistairmikeross@gmail.com>
2023-06-09 13:25:34 +0200

    New main

--- src/main/java/com/example/infosafe/app/src/Components/__tests__/Home.test.js
+++
@@ -1,6 +0,0 @@
-import Home from './../Home.js'
-import { render, screen, cleanup } from '@testing-library/react'
-
-test('should render Home component', () => {
-    render(<Home/>);
-})
--- src/main/java/com/example/infosafe/app/src/Components/__tests__/NavBar.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen, cleanup, fireEvent } from '@testing-library/react'
-import Nav from './../NavBar.js';
-
-test('should render Home component', () => {
-    render(<Nav/>);
-    const button = screen.getByRole('button', { name: 'Users'});
-})
