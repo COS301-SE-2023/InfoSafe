ce198b1d81e7499688fa65a2d7e0076c0cd15e10
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-06-09 20:47:25 +0200

    Created User entity

---
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/entities/User.java
@@ -0,0 +1,65 @@
+package com.example.infosafe_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Id
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String role;
+}
