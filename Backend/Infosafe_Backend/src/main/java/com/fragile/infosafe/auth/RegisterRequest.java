df1940a088324a8b2c50a575129c8d4783c40196
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-06-12 11:06:09 +0200

    Added authentication files

---
+++ Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.fragile.infosafe.auth;
+
+import com.fragile.infosafe.user.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
+  private Role role;
+}
