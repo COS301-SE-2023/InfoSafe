a9b93cdd08aac5f35c0527ce38e12cb2b58aa19b
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-06-13 20:41:44 +0200

    Added functions to datascopeService

--- Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/service/DataScopeServiceImpl.java
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/service/DataScopeServiceImpl.java
@@ -1,4 +1,18 @@
 package com.example.infosafe_backend.service;
 
-public class DataScopeServiceImpl {
+import com.example.infosafe_backend.model.DataScope;
+import com.example.infosafe_backend.model.User;
+import com.example.infosafe_backend.repository.DataScopeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataScopeServiceImpl implements DataScopeService {
+    @Autowired
+    private DataScopeRepository datascopeRepository;
+
+    @Override
+    public DataScope saveDataScope(DataScope datascope) {
+        return datascopeRepository.save(datascope);
+    }
 }
