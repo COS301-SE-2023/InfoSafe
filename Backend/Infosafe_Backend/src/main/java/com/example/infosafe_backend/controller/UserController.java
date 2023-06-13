80775998933e7243aa679bfb626d65655bf5117e
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-06-13 11:39:20 +0200

    Added User service files

---
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.infosafe_backend.controller;
+
+import com.example.infosafe_backend.model.User;
+import com.example.infosafe_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody User user){
+        userService.saveUser(user);
+        return "New user is added";
+    }
+
+}
--- Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/repository/UserRepository.java
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.infosafe_backend.repository;
 
-import com.example.infosafe_backend.model.Student;
+import com.example.infosafe_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student,Integer> {
+public interface UserRepository extends JpaRepository<User,Integer> {
+
 }
