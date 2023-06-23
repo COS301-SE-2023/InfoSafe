e51ce8bddcecc8549eaec6d65a76e12448a059a6
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-06-23 21:43:07 +0200

    Added support request design for ISO View implementation

--- Frontend/infosafe_frontend/src/Components/NavBar.js
+++ Frontend/infosafe_frontend/src/Components/NavBar.js
@@ -15,60 +15,91 @@ const NavBar = ({systemRole}) => {
             switch (activeNavTab) {
                 case 0:
                     const userItems = [];
-                    for (var i = 1; i < 30; i++) {
+                    for (let i = 1; i < 30; i++) {
                         userItems.push(<li key={i}>User {i}</li>);
                     }
 
                     return (
-
-                        <div className='users'>
+                        <div className="users">
                             <ul className="userList">{userItems}</ul>
                         </div>
                     );
                 case 1:
                     const dataItems = [];
-                    for (var i = 1; i < 30; i++) {
-                        dataItems.push(<li key={i}>Data Scope {i}</li>);
+                    for (let j = 1; j < 30; j++) {
+                        dataItems.push(<li key={j}>Data Scope {j}</li>);
                     }
 
                     return (
-
-                        <div className='datascopes'>
+                        <div className="datascopes">
                             <ul className="datascopesList">{dataItems}</ul>
                         </div>
                     );
                 case 2:
                     const accessRequests = [];
-                    for (var i = 1; i < 30; i++) {
-                        accessRequests.push(<li key={i}>Access Request {i}</li>);
+                    for (let k = 1; k < 30; k++) {
+                        accessRequests.push(<li key={k}>Access Request {k}</li>);
                     }
                     return (
-
-                        <div className='accessRequests'>
+                        <div className="accessRequests">
                             <ul className="accessrequestsList">{accessRequests}</ul>
                         </div>
                     );
                 case 3:
                     const complianceItems = [];
-                    for (var i=1; i < 30; i++) {
-                        complianceItems.push(<li key={i}>Task {i}</li>);
+                    for (let l = 1; l < 30; l++) {
+                        complianceItems.push(<li key={l}>Task {l}</li>);
                     }
                     return (
                         <div className="tasks">
                             <ul className="taskList">{complianceItems}</ul>
                         </div>
-
                     );
                 case 4:
                     const devices = [];
-                    for (var i = 0; i < 26; i++) {
-                        devices.push(<li key={i}>Device {String.fromCharCode(i + 65)}</li>);
+                    for (let m = 0; m < 26; m++) {
+                        devices.push(<li key={m}>Device {String.fromCharCode(m + 65)}</li>);
                     }
                     return (
-                        <div className='devices'>
-                            <ul className='deviceList'>{devices}</ul>
+                        <div className="devices">
+                            <ul className="deviceList">{devices}</ul>
                         </div>
                     );
+                case 5:
+                    const active_requests = [];
+                    for (let a = 1; a < 15; a++) {
+                        active_requests.push(<li key={a}>Support Request {a}</li>);
+                    }
+                    const my_requests = [];
+                    for (let b = 1; b < 15; b++) {
+                        my_requests.push(<li key={b}>Support Request {b}</li>);
+                    }
+
+                    return (
+                        <div>
+                            <div className="titles">
+                                <div className="activeHeader">
+                                    <p>Active System Requests</p>
+                                </div>
+                                <div className="myHeader">
+                                    <p>My Requests</p>
+                                </div>
+                            </div>
+
+                            <div className="tables">
+                                <div className="active_support_requests">
+                                    <ul className="activeRequestsList">{active_requests}</ul>
+                                </div>
+                                <div className="my_support_requests">
+                                    <ul className="myRequestsList">{my_requests}</ul>
+                                </div>
+                            </div>
+
+                        </div>
+                    );
+                case 6:
+                    //Add risks info here
+                    return null;
                 default:
                     return null;
             }
@@ -107,6 +138,12 @@ const NavBar = ({systemRole}) => {
                     return (
                         <button className='AddDeviceButton' onClick={() => console.log('Added new device')}>
                             Add Device
+                        </button>
+                    );
+                case 5:
+                    return (
+                        <button className='CreateSupportRequestButton' onClick={() => console.log('Create new support request.')}>
+                            Create New Request
                         </button>
                     );
                 default:
