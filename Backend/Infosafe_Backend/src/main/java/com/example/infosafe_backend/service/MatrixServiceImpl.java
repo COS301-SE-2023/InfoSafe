adb873c761283c65462976d1457edb86a529d202
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-06-24 16:28:47 +0200

    Added getAll Matrices to Spring

--- Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/service/MatrixServiceImpl.java
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/service/MatrixServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.infosafe_backend.repository.MatrixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MatrixServiceImpl implements MatrixService {
     @Autowired
@@ -13,5 +15,10 @@ public class MatrixServiceImpl implements MatrixService {
     @Override
     public Matrix saveMatrix(Matrix matrix) {
         return matrixRepository.save(matrix);
+    }
+
+    @Override
+    public List<Matrix> getAllMatrices() {
+        return matrixRepository.findAll();
     }
 }
