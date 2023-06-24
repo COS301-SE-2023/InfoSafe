c03bb280529ab2f2c4fd13c1dea2452224b6b3ef
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-06-24 21:52:57 +0200

    Improved Edit User implementation

--- Frontend/infosafe_frontend/src/Components/EditUser.js
+++ Frontend/infosafe_frontend/src/Components/EditUser.js
@@ -3,71 +3,67 @@ import React from 'react';
 import '../Styling/EditUser.css';
 import Dropdown from 'react-dropdown';
 /* eslint-disable react/prop-types */
- const EditUser = ({closedEditUser}) =>  {
+const EditUser = ({ closeEditUser, openEditUser }) => {
+    const role_options = [
+        'EMPLOYEE',
+        'ISO',
+        'DISO',
+        'DATA CUSTODIAN',
+        'SYSTEM ADMINISTRATOR',
+        'ASSET MANAGER'
+    ];
+    return (
+        <Popup open={openEditUser} onClose={closeEditUser} position="center center">
+            <div className="editUserOverlay">
+                <div className="border">
+                    <p className="editUserTitle">Edit User</p>
+                    <div className="nameEdit">
+                        <p className="nameTitle">Name</p>
+                        <input
+                            className="editNameInput"
+                            type="text"
+                            id="editusername"
+                            name="editusername"
+                            defaultValue="Jane"
+                        />
+                    </div>
+                    <div className="surnameEdit">
+                        <p className="surnameTitle">Surname</p>
+                        <input
+                            className="editSurnameInput"
+                            type="text"
+                            id="editusersurname"
+                            name="editusersurname"
+                            defaultValue="Doe"
+                        />
+                    </div>
+                    <div className="emailEdit">
+                        <p className="emailTitle">Email</p>
+                        <input
+                            className="editEmailInput"
+                            type="text"
+                            id="edituseremail"
+                            name="edituseremail"
+                            defaultValue="jane.doe@example.com"
+                        />
+                    </div>
+                    <div className="roleEdit">
+                        <p className="roleTitle">System Role</p>
+                        <Dropdown
+                            options={role_options}
+                            value={role_options[0]}
+                            className="roleDropdown"
+                            name="role"
+                        />
+                    </div>
+                    <button className="ChangePasswordButton">Change Password</button>
+                    <button className="FinishButton" onClick={() => console.log('Finish editing')}>
+                        Finish
+                    </button>
+                </div>
+            </div>
+        </Popup>
+    );
+};
 
-     const role_options = [
-         'EMPLOYEE',
-         'ISO',
-         'DISO',
-         'DATA CUSTODIAN',
-         'SYSTEM ADMINISTRATOR',
-         'ASSET MANAGER'
-     ];
-     return (
-         <Popup open={true} onClose={closeEditUser} position="center center">
-             <div className="editUserOverlay">
-                 <div className="border">
-                     <p className="editUserTitle">Edit User</p>
-                     <div className="nameEdit">
-                         <p className="nameTitle">Name</p>
-                         <input
-                             className="editNameInput"
-                             type="text"
-                             id="editusername"
-                             name="editusername"
-                             defaultValue="Jane"
-                         />
-                     </div>
-                     <div className="surnameEdit">
-                         <p className="surnameTitle">Surname</p>
-                         <input
-                             className="editSurnameInput"
-                             type="text"
-                             id="editusersurname"
-                             name="editusersurname"
-                             defaultValue="Doe"
-                         />
-                     </div>
-                     <div className="emailEdit">
-                         <p className="emailTitle">Email</p>
-                         <input
-                             className="editEmailInput"
-                             type="text"
-                             id="edituseremail"
-                             name="edituseremail"
-                             defaultValue="jane.doe@example.com"
-                         />
-                     </div>
-                     <div className="roleEdit">
-                         <p className="roleTitle">System Role</p>
-                         <Dropdown
-                             options={role_options}
-                             value={role_options[0]}
-                             className="roleDropdown"
-                             name="role"
-                         />
-                     </div>
-                     <button className="ChangePasswordButton">Change Password</button>
-                     <button
-                         className="FinishButton"
-                         onClick={() => console.log('Finish editing')}
-                     >
-                         Finish
-                     </button>
-                 </div>
-             </div>
-         </Popup>
-     );
- };
-
- export default EditUser;
+export default EditUser;
