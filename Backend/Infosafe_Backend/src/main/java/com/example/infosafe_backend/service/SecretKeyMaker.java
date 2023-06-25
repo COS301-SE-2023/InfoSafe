33fb14d0fc80cabe64d2628501288f2487696727
Chris Mittendorf <u15092462@tuks.co.za>
2023-06-25 10:18:58 +0200

    SK Maker impl file
    The implementation file for the secret key maker

--- Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/service/SecretKeyMaker.java
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/service/SecretKeyMaker.java
@@ -1,16 +1,9 @@
 package com.example.infosafe_backend.service;
 
 import java.io.UnsupportedEncodingException;
-import java.security.AlgorithmParameters;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.util.Base64;
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public interface SecretKeyMaker {
---
+++ Backend/Infosafe_Backend/src/main/java/com/example/infosafe_backend/service/SecretKeyMakerImpl.java
@@ -0,0 +1,55 @@
+package com.example.infosafe_backend.service;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.AlgorithmParameters;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.util.Base64;
+
+public class SecretKeyMakerImpl implements SecretKeyMaker{
+    @Override
+    public SecretKeySpec generateSecretKey(char[] password, byte[] salt, int iterations, int keyLength) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterations,keyLength);
+
+        SecretKey tempKey = keyFactory.generateSecret(keySpec);
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(tempKey.getEncoded(), "AES");
+
+        return secretKeySpec;
+    }
+
+
+    @Override
+    public String encrypt(String dataToEncrypt) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidParameterSpecException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
+
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        AlgorithmParameters parameters = pbeCipher.getParameters();
+
+        IvParameterSpec ivParameterSpec = parameters.getParameterSpec(IvParameterSpec.class);
+
+        byte[] cryptoText = pbeCipher.doFinal(dataToEncrypt.getBytes("UTF-8"));
+
+        byte[] iv = ivParameterSpec.getIV();
+
+        String encryptedData = base64Encoder(iv) + ":" +base64Encoder(cryptoText);
+
+        return encryptedData;
+    }
+
+    @Override
+    public String base64Encoder(byte[] bytes) {
+
+        String encoder = Base64.getEncoder().encodeToString(bytes);
+
+        return encoder;
+    }
+}
