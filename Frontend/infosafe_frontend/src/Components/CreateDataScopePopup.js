b630b42639e0d7b154a0f0a4d16f3b9e2d61fc49
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-06-26 16:45:10 +0200

    Updated datascope creation

--- Frontend/infosafe_frontend/src/Components/CreateDataScopePopup.js
+++ Frontend/infosafe_frontend/src/Components/CreateDataScopePopup.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../Styling/CreateDataScopePopup.css';
 import Popup from 'reactjs-popup';
 import { IoArrowBackOutline } from 'react-icons/io5';
 
 export const CreateDataScopePopup = ({ popupOpen, popupClose }) => {
+    const current = new Date();
+    const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`;
+
+    const[ds_name,setDsName]=useState('')
+    const[ds_description,setDsDesc]=useState('')
+    const[role_name,setRoleName]=useState('General User')
+    const[role_description,setRoleDesc]=useState('Can use basic functionality of the product')
+    const[date_captured,setDateCaptured]=useState(date)
+    const[data_custodian,setDataCustodian]=useState('Alistair Ross')
+    const[administrator,setAdmin]=useState('Admin1')
+    const[status,setStatus]=useState('Pending Approval')
+
+    const handleClick=(e)=> {
+        e.preventDefault()
+        const datascope = {ds_name, ds_description, role_name, role_description, date_captured, data_custodian, administrator, status}
+        console.log(datascope)
+        fetch("http://localhost:8080/api/auth/datascope/add", {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(datascope)
+        }).then(()=>{
+            console.log("New Data-Scope added")
+        })
+        popupClose()
+    }
+
     return (
         <Popup open={popupOpen} closeOnDocumentClick={false} position="center center">
             <div className="createDataScopeOverlay">
@@ -18,7 +44,7 @@ export const CreateDataScopePopup = ({ popupOpen, popupClose }) => {
                         <p className="descriptionLabel">Description</p>
                         <textarea className="descriptionInput" />
                         <br />
-                        <button className="datascope_finish" onClick={popupClose}>
+                        <button className="datascope_finish" onClick={handleClick}>
                             Submit
                         </button>
                     </form>
