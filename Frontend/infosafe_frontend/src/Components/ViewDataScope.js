2f43daf53ce38328cc1f1748f46e419cdd8e881c
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-06-26 16:56:58 +0200

    Updated Data Scopes Display, Information, Roles

--- Frontend/infosafe_frontend/src/Components/ViewDataScope.js
+++ Frontend/infosafe_frontend/src/Components/ViewDataScope.js
@@ -6,6 +6,17 @@ import { IoArrowBackOutline } from 'react-icons/io5';
 /* eslint-disable react/prop-types */
 
 const ViewDataScope = ({ id, popupClose, popupOpen }) => {
+    const roles = [
+        {
+            role: 'Administrator',
+            roledescription: 'Manage users, manage data scope, edit permissions.'
+        },
+        {
+            role: 'General User',
+            roledescription: 'Access data scope, complete tasks within data scopes.'
+        }
+    ];
+
     return (
         <Popup open={popupOpen} closeOnDocumentClick={false} position="center center">
             <div className="viewDataScopeOverlay">
@@ -32,19 +43,27 @@ const ViewDataScope = ({ id, popupClose, popupOpen }) => {
                     </div>
                     <div className="view_datascope_roles">
                         <p className="datascopeRoles">Roles</p>
-                        <div className="roleView">
-                            <div className="headers">
-                                <p className="header_1">Role</p>
-                                <p className="header_2">Role Description</p>
-                            </div>
-                            <div className="role_info">
-                                <p className="role_name">Administrator</p>
-                                <p className="role_descr">Manage users, manage data scope, edit permissions.</p>
-                            </div>
-                            <div className="role_info">
-                                <p className="role_name">General User</p>
-                                <p className="role_name">Access data scope, complete tasks within data scopes.</p>
-                            </div>
+                        <div className="table">
+                            <table className="rolesTable">
+                                <thead>
+                                    <tr>
+                                        <th className="roleHeader">Role</th>
+                                        <th className="roledescrHeader">Role Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {roles.map((roles, key) => {
+                                        return (
+                                            <tr key={key}>
+                                                <td>{roles.role}</td>
+                                                <td className="roledescriptionTable">
+                                                    {roles.roledescription}
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
