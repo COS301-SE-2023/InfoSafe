73ca6132292a91f9e68d9cf5413f885c8cd295c0
Chris Mittendorf <u15092462@tuks.co.za>
2023-07-16 22:18:42 +0200

    Compliance Matrix DB Updates
    Compliance Matrix name and schema updates for RDS database

--- Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/repository/ComplianceMatrixRepository.java
+++ Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/repository/ComplianceMatrixRepository.java
@@ -1,10 +1,10 @@
 package com.fragile.infosafe.repository;
 
-import com.fragile.infosafe.model.Matrix;
+import com.fragile.infosafe.model.ComplianceMatrix;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MatrixRepository extends JpaRepository<Matrix,Integer> {
+public interface ComplianceMatrixRepository extends JpaRepository<ComplianceMatrix,Integer> {
 
 }
