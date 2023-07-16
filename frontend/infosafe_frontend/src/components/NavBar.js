7b447548927e375d03d3cc05aac75fe15f103599
Karel-Louis Smit <u20447932@tuks.co.za>
2023-07-16 23:40:30 +0200

    Initial implementation for view task popup

--- frontend/infosafe_frontend/src/components/NavBar.js
+++ frontend/infosafe_frontend/src/components/NavBar.js
@@ -17,6 +17,7 @@ import { UpdateTask } from './UpdateTaskPopup';
 import ViewAccessRequest from './ViewAccessRequest';
 import EditAccessRequest from './EditAccessRequest';
 import Requests from './Requests';
+import {ViewTask} from "./ViewTaskPopup";
 
 /* eslint-disable react/prop-types */
 const NavBar = ({ systemRole }) => {
@@ -31,6 +32,7 @@ const NavBar = ({ systemRole }) => {
     const [updateTaskOpen, setUpdateTaskOpen] = useState(false);
     const [viewAccessRequestOpen, setViewAccessRequestOpen] = useState(false);
     const [editAccessRequestOpen, setEditAccessRequestOpen] = useState(false);
+    const [viewTaskOpen, setViewTaskOpen] = useState(false);
     const handleClick = (NavTabIndex) => {
         activate(NavTabIndex);
     };
@@ -157,6 +159,23 @@ const NavBar = ({ systemRole }) => {
         );
     };
 
+/*    const ViewTaskItem = ( l ) => {
+        const [viewTaskOpen, setViewTaskOpen] = useState(false);
+        return (
+            <li key={l}>
+                <p onClick={() => setViewTaskOpen(!viewTaskOpen)}>
+                    Task {l}
+                    {viewTaskOpen && (
+                        <ViewTask
+                            popupClose={() => setViewTaskOpen(false)}
+                            popupOpen={viewTaskOpen}
+                        />
+                    )}
+                </p>
+            </li>
+        );
+    };*/
+
     const displayPage = () => {
         if (systemRole === 'ISO') {
             return (
@@ -167,208 +186,6 @@ const NavBar = ({ systemRole }) => {
             );
         }
     };
-
-    /*    const displayInfo = () => {
-        if (systemRole === 'ISO') {
-            switch (activeNavTab) {
-                case 0: {
-                    const userItems = [];
-                    showUser.map((user) =>
-                        userItems.push(<ViewUserItem user={user} key={user.id}/>)
-                    );
-
-                    return (
-                        <div className="users">
-                            <ul className="userList">{userItems}</ul>
-                        </div>
-                    );
-                }
-                case 1: {
-                    const dataItems = [];
-                    showDatascope.map((datascope) =>
-                        dataItems.push(
-                            <ViewDataScopeItem datascope={datascope} key={datascope.id}/>
-                        )
-                    );
-
-                    return (
-                        <div className="datascopes">
-                            <ul className="datascopesList">{dataItems}</ul>
-                        </div>
-                    );
-                }
-                case 2: {
-                    const accessRequests = [];
-                    for (let k = 1; k < 30; k++) {
-                        accessRequests.push(
-                            <li key={k}>
-                                Access Request {k} <FaRegEdit className="EditIcon"/>{' '}
-                                <RiDeleteBin6Fill className="DeleteIcon"/>
-                            </li>
-                        );
-                    }
-                    return (
-                        <div className="accessRequests">
-                            <ul className="accessrequestsList">{accessRequests}</ul>
-                        </div>
-                    );
-                }
-                case 3: {
-                    const complianceItems = [];
-                    for (let l = 1; l < 30; l++) {
-                        complianceItems.push(<li key={l}>Task {l}</li>);
-                    }
-                    return (
-                        <div className="tasks">
-                            <ul className="taskList">{complianceItems}</ul>
-                        </div>
-                    );
-                }
-                case 4: {
-                    const devices = [];
-                    showAsset.map((device) =>
-                        devices.push(<ViewDeviceItem asset={device} key={device.id}/>)
-                    );
-                    return (
-                        <div className="devices">
-                            <ul className="deviceList">{devices}</ul>
-                        </div>
-                    );
-                }
-                case 5: {
-                    const active_requests = [];
-                    for (let a = 1; a < 15; a++) {
-                        active_requests.push(
-                            <li key={a}>
-                                Support Request {a} <FaRegEdit className="EditIcon"/>
-                            </li>
-                        );
-                    }
-                    const my_requests = [];
-                    for (let b = 1; b < 15; b++) {
-                        my_requests.push(
-                            <li key={b}>
-                                Support Request {b} <FaRegEdit className="EditIcon"/>
-                            </li>
-                        );
-                    }
-
-                    return (
-                        <div>
-                            <div className="titles">
-                                <div className="activeHeader">
-                                    <p>Active System Requests</p>
-                                </div>
-                                <div className="myHeader">
-                                    <p>My Requests</p>
-                                </div>
-                            </div>
-
-                            <div className="tables">
-                                <div className="active_support_requests">
-                                    <ul className="activeRequestsList">{active_requests}</ul>
-                                </div>
-                                <div className="my_support_requests">
-                                    <ul className="myRequestsList">{my_requests}</ul>
-                                </div>
-                            </div>
-                        </div>
-                    );
-                }
-                case 6:
-                    //Add risks info here
-                    return null;
-                default:
-                    return null;
-            }
-        }
-    };
-
-    const displayButtons = () => {
-        if (systemRole === 'ISO') {
-            switch (activeNavTab) {
-                case 0:
-                    return (
-                        <div className="CreateUserButtonDiv">
-                            <button
-                                className="CreateUserButton"
-                                data-testid="CreateUserButton"
-                                onClick={() => setCreateUserOpen(true)}
-                            >
-                                Create New User
-                            </button>
-                            {createUserOpen ? (
-                                <CreateUserPopup
-                                    popupClose={() => setCreateUserOpen(false)}
-                                    popupOpen={createUserOpen}
-                                />
-                            ) : null}
-                        </div>
-                    );
-                case 1:
-                    return (
-                        <div className="CreateDataScopeDiv">
-                            <button
-                                className="CreateDataScopeButton"
-                                onClick={() => setCreateDataScopeOpen(true)}
-                            >
-                                Create Data Scope
-                            </button>
-                            {createDataScopeOpen ? (
-                                <CreateDataScopePopup
-                                    popupClose={() => setCreateDataScopeOpen(false)}
-                                    popupOpen={createDataScopeOpen}
-                                />
-                            ) : null}
-                        </div>
-                    );
-                case 3:
-                    return (
-                        <div className="buttons">
-                            <button
-                                className="CreateTaskButton"
-                                onClick={() => console.log('Created new task')}
-                            >
-                                Create New Task
-                            </button>
-                            <button
-                                className="UpdateTaskButton"
-                                onClick={() => console.log('Updated task.')}
-                            >
-                                Update Task
-                            </button>
-                            <button
-                                className="RevokeTaskButton"
-                                onClick={() => console.log('Revoked task.')}
-                            >
-                                Revoke Task
-                            </button>
-                        </div>
-                    );
-                case 4:
-                    return (
-                        <div className="AddDeviceDiv">
-                            <button
-                                className="AddDeviceButton"
-                                onClick={() => setCreateDeviceOpen(true)}
-                            >
-                                Add Device
-                            </button>
-                            {createDeviceOpen ? (
-                                <CreateDevicePopup
-                                    popupClose={() => setCreateDeviceOpen(false)}
-                                    popupOpen={createDeviceOpen}
-                                />
-                            ) : null}
-                        </div>
-                    );
-                case 5:
-                    return null;
-                default:
-                    return null;
-            }
-        }
-    };*/
 
     const displayISOTabs = () => {
         return (
@@ -496,7 +313,17 @@ const NavBar = ({ systemRole }) => {
             case 3: {
                 const complianceItems = [];
                 for (let l = 1; l < 30; l++) {
-                    complianceItems.push(<li key={l}>Task {l}</li>);
+                    complianceItems.push( <li key={l}>
+                        <p onClick={() => setViewTaskOpen(true)}>
+                            Task {l}
+                            {viewTaskOpen ? (
+                                <ViewTask
+                                    popupClose={() => setViewTaskOpen(false)}
+                                    popupOpen={viewTaskOpen}
+                                />
+                            ) : null}
+                        </p>
+                    </li>);
                 }
                 return (
                     <div className="display">
@@ -645,38 +472,6 @@ const NavBar = ({ systemRole }) => {
     };
 
     return displayPage();
-    /*return (
-        <div className="navbar">
-            <ul className="tabs">
-                <li className={activeNavTab === 0 ? 'active' : ''} onClick={() => handleClick(0)}>
-                    Users
-                </li>
-                <li className={activeNavTab === 1 ? 'active' : ''} onClick={() => handleClick(1)}>
-                    Data Scopes
-                </li>
-                <li className={activeNavTab === 2 ? 'active' : ''} onClick={() => handleClick(2)}>
-                    Access Requests
-                </li>
-                <li className={activeNavTab === 3 ? 'active' : ''} onClick={() => handleClick(3)}>
-                    Compliance Matrix
-                </li>
-                <li className={activeNavTab === 4 ? 'active' : ''} onClick={() => handleClick(4)}>
-                    Devices
-                </li>
-                <li className={activeNavTab === 5 ? 'active' : ''} onClick={() => handleClick(5)}>
-                    Support Requests
-                </li>
-                <li className={activeNavTab === 6 ? 'active' : ''} onClick={() => handleClick(6)}>
-                    Risks
-                </li>
-            </ul>
-
-            <div className="display">
-                {displayInfo()}
-                {displayButtons()}
-            </div>
-        </div>
-    );*/
 };
 
 export default NavBar;
---
+++ frontend/infosafe_frontend/src/components/ViewTaskPopup.js
@@ -0,0 +1,28 @@
+import Popup from 'reactjs-popup';
+import React from 'react';
+import '../styling/ViewTask.css';
+import {IoArrowBackOutline} from 'react-icons/io5';
+/* eslint-disable react/prop-types */
+/* eslint-disable  no-unused-vars */
+export const ViewTask = ({popupClose, popupOpen}) => {
+    return (
+        <Popup open={popupOpen} closeOnDocumentClick={false}>
+            <div className="viewTaskOverlay">
+                <div className="borderViewTask">
+                    <button className="backButton" onClick={popupClose}>
+                        <IoArrowBackOutline className="backIcon"/>
+                    </button>
+                    <p className="pageTitle">View Task</p>
+                    <p className="displayTitle">Task ID</p>
+                    <p className="displayData">Task 123</p>
+                    <p className="displayTitle">Data Scope</p>
+                    <p className="displayData">Data Scope A</p>
+                    <p className="displayTitle">Task Description</p>
+                    <textarea className="viewTextArea" readOnly={true}>Example of a task description. </textarea>
+                    <p className="displayTitle">Completion Date</p>
+                    <p className="displayData">12/12/2012</p>
+                </div>
+            </div>
+        </Popup>
+    );
+};
