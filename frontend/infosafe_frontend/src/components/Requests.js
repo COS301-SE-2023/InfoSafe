7abad52c1217d569c197aa0a517677c095e51134
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-07-19 22:42:09 +0200

    Updated DISO request options.

--- frontend/infosafe_frontend/src/components/Requests.js
+++ frontend/infosafe_frontend/src/components/Requests.js
@@ -83,6 +83,7 @@ const displayISORequests = () => {
 };
 
 const displayDISORequests = () => {
+    const REQUESTTYPES = ['Support Request', 'Asset Request'];
     const SUPPORTOPTIONS = [
         'Laptop Hardware',
         'Microsoft Accounts',
@@ -90,33 +91,70 @@ const displayDISORequests = () => {
         'Application',
         'Other'
     ];
+    const [selectedRequest, setSelectedRequest] = useState(REQUESTTYPES[0]);
+    const handleRequestSelect = (requestType) => {
+        setSelectedRequest(requestType.value);
+    };
     return (
         <div className="display">
-            <div className="onlySupportRequestDiv">
-                <form>
-                    <div className="createSupportRequestContentDiv">
-                        <p className="createSupportRequestLabel">Create Support Request</p>
-
-                        <p className="createSupportRequestTypeLabel">Support Type</p>
-                        <Dropdown
-                            className="createSupportRequestTypeDropdown"
-                            options={SUPPORTOPTIONS}
-                            value={SUPPORTOPTIONS[0]}
-                        />
-                        <p className="createSupportRequestDescriptionLabel">Description</p>
-                        <textarea className="createSupportRequestDescription"></textarea>
+            <div className="selectRequestDiv">
+                <div className="selectRequest">
+                    <p className="selectRequestLabel">Select Type of Request</p>
+                    <Dropdown
+                        className="selectRequestDropdown"
+                        options={REQUESTTYPES}
+                        value={selectedRequest}
+                        onChange={handleRequestSelect}
+                    />
+                </div>
+            </div>
+            <div className="requestDiv">
+                {selectedRequest === 'Support Request' && (
+                    <div className="createSupportRequestDiv">
+                        <form>
+                            <p className="supportRequestTypeLabel">Support Type</p>
+                            <Dropdown
+                                className="supportRequestTypeDropdown"
+                                options={SUPPORTOPTIONS}
+                                value={SUPPORTOPTIONS[0]}
+                            />
+                            <p className="supportRequestDescriptionLabel">Description</p>
+                            <textarea className="supportRequestDescription"></textarea>
+                            <div className="createSupportRequestButtonDiv">
+                                <button
+                                    className="createSupportRequestButton"
+                                    type="submit"
+                                    onClick={() => console.log('Create Support Request')}
+                                >
+                                    Log Request
+                                </button>
+                            </div>
+                        </form>
                     </div>
-
-                    <div className="supportRequestButtonDiv">
-                        <button
-                            className="supportRequestButton"
-                            type="submit"
-                            onClick={() => console.log('Create Support Request')}
-                        >
-                            Log Request
-                        </button>
+                )}
+                {selectedRequest === 'Asset Request' && (
+                    <div className="createAssetRequestDiv">
+                        <form>
+                            <p className="createAssetRequestDeviceNameLabel">Device Name</p>
+                            <input type="text" className="createAssetRequestDeviceNameInput" />
+                            <p className="createAssetRequestUserLabel">User</p>
+                            <input type="text" className="createAssetRequestUserInput" />
+                            <p className="createAssetRequestReasonLabel">Reason</p>
+                            <textarea className="createAssetRequestReasonInput"></textarea>
+                            <p className="createAssetRequestDateLabel">Date Required</p>
+                            <input type="text" className="createAssetRequestDateInput" />
+                            <div className="createAssetRequestButtonDiv">
+                                <button
+                                    className="createAssetRequestButton"
+                                    type="submit"
+                                    onClick={() => console.log('Create Asset Request')}
+                                >
+                                    Log Request
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                )}
             </div>
         </div>
     );
