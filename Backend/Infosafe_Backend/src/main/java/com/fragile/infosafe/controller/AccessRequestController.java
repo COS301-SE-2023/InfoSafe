98d8b4e87966c84ef348c50fa8ff065ab6f20f24
P3TROOS <56771007+P3TROOS@users.noreply.github.com>
2023-07-19 18:50:37 +0200

    Created controller for AccessRequests

---
+++ Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/controller/AccessRequestController.java
@@ -0,0 +1,27 @@
+package com.fragile.infosafe.controller;
+
+import com.fragile.infosafe.model.AccessRequest;
+import com.fragile.infosafe.requests.AccessRequestRequest;
+import com.fragile.infosafe.service.AccessRequestService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Slf4j
+@RestController
+@RequestMapping("api/accessrequest")
+@RequiredArgsConstructor
+//@CrossOrigin
+public class AccessRequestController {
+    private final AccessRequestService service;
+    @PostMapping("/addAr")
+    public ResponseEntity addAr(@RequestBody AccessRequestRequest accessrequest) {
+        log.info("Adding an access request");
+        return ResponseEntity.ok(service.makeAR(accessrequest));
+    }
+
+    @GetMapping("/getAr")
+    public List<AccessRequest> accessrequestlist() { return service.getAllAccessRequests(); }
+}
