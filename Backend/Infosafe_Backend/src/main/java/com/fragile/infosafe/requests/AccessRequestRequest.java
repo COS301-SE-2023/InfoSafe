a6aa34a0c2edb47aeb986f099d929454967ca806
P3TROOS <56771007+P3TROOS@users.noreply.github.com>
2023-07-19 17:31:46 +0200

    Created Requests for AccessRequests

---
+++ Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/requests/AccessRequestRequest.java
@@ -0,0 +1,18 @@
+package com.fragile.infosafe.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccessRequestRequest {
+    private int request_id;
+    private int user_id;
+    private int ds_id;
+}
