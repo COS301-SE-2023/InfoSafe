999490c5fb33297e7573a3d91b5f1a9ed84ba780
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2023-07-27 12:28:55 +0200

    Removing unnecessary code

--- backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/service/PasswordEncryption.java
+++
@@ -1,8 +0,0 @@
-package com.fragile.infosafe.service;
-
-import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-
-public interface PasswordEncryption {
-    public String encryptPassword(String password) throws GeneralSecurityException, UnsupportedEncodingException;
-}
