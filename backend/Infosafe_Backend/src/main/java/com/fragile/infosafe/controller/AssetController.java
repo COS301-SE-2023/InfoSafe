a88f9252713506f26a1d152feddd3563326f0264
P3TROOS <56771007+P3TROOS@users.noreply.github.com>
2023-07-28 08:34:38 +0200

    Added endpoints for editing assets
    Broken at the moment. Need to clarify model fields with team.

--- backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/controller/AssetController.java
+++ backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/controller/AssetController.java
@@ -4,21 +4,30 @@ import com.fragile.infosafe.model.Asset;
 import com.fragile.infosafe.requests.AssetRequest;
 import com.fragile.infosafe.service.AssetService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("api/asset")
 @RequiredArgsConstructor
+//@CrossOrigin
 public class AssetController {
     private final AssetService service;
     @PostMapping("/addAsset")
-    public ResponseEntity addAsset(@RequestBody AssetRequest asset){
+    public ResponseEntity addAsset(@RequestBody AssetRequest asset) {
+        log.info("Adding an asset");
         return ResponseEntity.ok(service.makeAsset(asset));
     }
 
     @GetMapping("/getAsset")
-    public List<Asset> list() { return service.getAllAssets(); }
+    public List<Asset> assetlist() { return service.getAllAssets(); }
+
+    @PutMapping("/update/{id}")
+    public Asset updateAsset (@PathVariable("id") String serial_number, @RequestBody Asset asset) {
+        asset.setSerial_number(serial_number);
+        return service.updateAsset(asset);
+    }
 }
