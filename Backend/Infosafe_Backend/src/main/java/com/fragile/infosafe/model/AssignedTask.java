b62a0cc4c5bbbbd867fdf7819786b0b4b84ee707
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2023-07-31 10:08:56 +0200

    Fixed assigned task

--- Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/model/AssignedTask.java
+++ Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/model/AssignedTask.java
@@ -2,6 +2,7 @@ package com.fragile.infosafe.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@IdClass(AssignedTaskId.class)
 @Table(name="assignedtask")
 public class AssignedTask {
     public Long getUser_Id() {
