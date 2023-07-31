367731b4757d7ce99bbe0e80cbbfcd8de049a116
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2023-08-01 01:17:52 +0200

    Fixing linting

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '8o48nn',
+  projectId: "8o48nn",
   e2e: {
     baseUrl: "http://localhost:8080",
     viewportWidth: 1024,
