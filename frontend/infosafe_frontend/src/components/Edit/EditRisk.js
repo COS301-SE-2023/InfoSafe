36fdda105b5da5197d2e017f61204c59a623047f
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-07-31 17:36:28 +0200

    Added edits for risks

--- frontend/infosafe_frontend/src/components/Edit/EditRisk.js
+++ frontend/infosafe_frontend/src/components/Edit/EditRisk.js
@@ -1,5 +1,5 @@
 import Popup from 'reactjs-popup';
-import React from 'react';
+import React, {useState} from 'react';
 import '../../styling/EditRisk.css';
 import { IoArrowBackOutline } from 'react-icons/io5';
 import Dropdown from 'react-dropdown';
@@ -7,7 +7,34 @@ import Dropdown from 'react-dropdown';
 /* eslint-disable  no-unused-vars */
 const PROBABILITY = ['Almost Certain', 'Likely', 'Moderate','Unlikely','Rare'];
 const IMPACT = ['Insignificant','Minor','Significant','Major','Severe'];
-export const EditRisk = ({ popupClose, popupOpen }) => {
+export const EditRisk = ({ risk, popupClose, popupOpen }) => {
+
+    const[values, setValues]=useState({
+        risk_id: risk.risk_id,
+        ds_id: risk.ds_id,
+        risk_description: risk.risk_description,
+        impact_rating: risk.impact_rating,
+        probability_rating: risk.probability_rating,
+        suggested_mitigation: risk.suggested_mitigation,
+        risk_status: risk.risk_status
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(values)
+        fetch('http://localhost:8080/api/risk/update/' + risk.risk_id, {
+            method:"PUT",
+            headers:{"Content-Type":"application/json",
+                Authorization: "Bearer " + sessionStorage.getItem('accessToken')
+            },
+            body:JSON.stringify(values)
+        }).then(()=>{
+            console.log("Updated Asset")
+        })
+        //console.log(JSON.stringify(values))
+        popupClose()
+    }
+
     return (
         <Popup open={popupOpen} closeOnDocumentClick={false}>
             <div className="editRiskOverlay">
@@ -15,30 +42,36 @@ export const EditRisk = ({ popupClose, popupOpen }) => {
                     <button className="backButton" onClick={popupClose}>
                         <IoArrowBackOutline className="backIcon" />
                     </button>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <p className="pageTitle">Edit Risk</p>
                         <p className="displayTitle">Data Scope</p>
-                        <p className="displayData">Data Scope A</p>
+                        <p className="displayData">{risk.ds_id}</p>
                         <p className="inputTitle">Probability</p>
                         <Dropdown
                             options={PROBABILITY}
-                            value={PROBABILITY[0]}
+                            value={risk.probability_rating}
                             className="probabilityDropdown"
                             name="probabilityDropdown"
+                            onChange={(selectedOption) => setValues({...values, probability_rating: selectedOption.value})}
                         />
                         <p className="inputTitle">Impact</p>
                         <Dropdown
                             options={IMPACT}
-                            value={IMPACT[0]}
+                            value={risk.impact_rating}
                             className="impactDropdown"
                             name="impactDropdown"
+                            onChange={(selectedOption) => setValues({...values, impact_rating: selectedOption.value})}
                         />
-                        <p className="inputTitle">Vulnerability</p>
-                        <textarea className="inputTextArea" />
-                        <p className="inputTitle">Threat</p>
-                        <textarea className="inputTextArea" />
+                        <p className="inputTitle">Vulnerability/Threat</p>
+                        <textarea
+                            className="inputTextArea"
+                            defaultValue={risk.risk_description}
+                            onChange={e => setValues({...values, risk_description: e.target.value})}
+                        />
+                        <!--<p className="inputTitle">Threat</p>
+                        <textarea className="inputTextArea" />-->
                         <div>
-                            <button className="submitButton" type="submit" onClick={popupClose}>
+                            <button className="submitButton" type="submit">
                                 Submit
                             </button>
                         </div>
