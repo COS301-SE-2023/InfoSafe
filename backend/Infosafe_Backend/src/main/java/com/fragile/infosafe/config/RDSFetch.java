eceac1ab87f1746ea2a9f51d53b81771b103ee45
Chris Mittendorf <u15092462@tuks.co.za>
2023-08-30 11:03:39 +0200

    RDS login function

---
+++ backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/config/RDSFetch.java
@@ -0,0 +1,77 @@
+package com.fragile.infosafe.config;
+
+import com.google.gson.Gson;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import javax.sql.DataSource;
+
+@Configuration
+public class RDSFetch {
+
+    private Gson gson = new Gson();
+
+    @Bean
+    public DataSource datasource() {
+
+        RDSLogin login = getRDSLogin();
+
+        return DataSourceBuilder
+                .create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:" + login.getEngine() + "://" + login.getHost() + ":" + login.getPort() + "/" + login.getDbname())
+                .username(login.getUsername())
+                .password(login.getPassword())
+                .build();
+    }
+
+    private RDSLogin getRDSLogin() {
+
+        String secretName = "rds_login";
+        String region = "us-east-1";
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(new AwsCredentialsProvider() {
+                    @Override
+                    public AwsCredentials resolveCredentials() {
+                        AwsCredentials awsCredentials = new AwsCredentials() {
+                            @Override
+                            public String accessKeyId() {
+                                return "AKIAZ4AZMVOI6GKFHR4W";
+                            }
+
+                            @Override
+                            public String secretAccessKey() {
+                                return "wupdTJ6zNErPCaLb7nqORCjgAGW9/YmzwsRk2lsD";
+                            }
+                        };
+
+                        return awsCredentials;
+                    }
+                })
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        String secret = getSecretValueResponse.secretString();
+        return gson.fromJson(secret, RDSLogin.class);
+    }
+}
