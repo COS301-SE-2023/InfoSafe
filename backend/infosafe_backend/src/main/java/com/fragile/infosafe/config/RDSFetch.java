dde768a691cfb97dc8e45e370565dd5bb4f87b53
Chris Mittendorf <u15092462@tuks.co.za>
2023-08-30 12:05:52 +0200

    All Aws updates readded

---
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/config/RDSFetch.java
@@ -0,0 +1,129 @@
+package com.fragile.infosafe.config;
+
+import com.google.gson.Gson;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import javax.sql.DataSource;
+
+@Configuration
+public class RDSFetch {
+
+    private Gson gson = new Gson();
+
+    @Bean
+    public DataSource datasource() {
+
+        RDSLogin login = getRDSLogin();
+
+        return DataSourceBuilder
+                .create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:" + login.getEngine() + "://" + login.getHost() + ":" + login.getPort() + "/" + login.getDbname())
+                .username(login.getUsername())
+                .password(login.getPassword())
+                .build();
+    }
+
+    private RDSLogin getRDSLogin() {
+
+        String secretName = "rds_login";
+        String region = "us-east-1";
+        //String accessKey = getAccess();
+        //String secretKey = getSecret();
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(new AwsCredentialsProvider() {
+                    @Override
+                    public AwsCredentials resolveCredentials() {
+                        AwsCredentials awsCredentials = new AwsCredentials() {
+                            @Override
+                            public String accessKeyId() {
+                                return "";
+                            }
+
+                            @Override
+                            public String secretAccessKey() {
+                                return "";
+                            }
+                        };
+
+                        return awsCredentials;
+                    }
+                })
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        String secret = getSecretValueResponse.secretString();
+        return gson.fromJson(secret, RDSLogin.class);
+    }
+
+//    private String getAccess() {
+//
+//        String secretName = "security_credentials";
+//        Region region = Region.of("us-east-1");
+//
+//        // Create a Secrets Manager client
+//        SecretsManagerClient client = SecretsManagerClient.builder()
+//                .region(region)
+//                .build();
+//
+//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+//                .secretId(secretName)
+//                .build();
+//
+//        GetSecretValueResponse getSecretValueResponse;
+//
+//        try {
+//            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+//        } catch (Exception e) {
+//            throw e;
+//        }
+//
+//        return getSecretValueResponse.secretString();
+//    }
+//
+//    private String getSecret() {
+//
+//        String secretName = "security_access_secret";
+//        Region region = Region.of("us-east-1");
+//
+//        // Create a Secrets Manager client
+//        SecretsManagerClient client = SecretsManagerClient.builder()
+//                .region(region)
+//                .build();
+//
+//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+//                .secretId(secretName)
+//                .build();
+//
+//        GetSecretValueResponse getSecretValueResponse;
+//
+//        try {
+//            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+//        } catch (Exception e) {
+//            throw e;
+//        }
+//
+//        return getSecretValueResponse.secretString();
+//    }
+}
