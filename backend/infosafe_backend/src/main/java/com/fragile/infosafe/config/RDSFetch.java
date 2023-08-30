be72400296a6ae9e01041a1c1d1b4a6e2777d46e
Chris Mittendorf <u15092462@tuks.co.za>
2023-08-30 14:14:05 +0200

    Update RDSFetch.java

--- backend/infosafe_backend/src/main/java/com/fragile/infosafe/config/RDSFetch.java
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/config/RDSFetch.java
@@ -1,15 +1,23 @@
 package com.fragile.infosafe.config;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
 import com.google.gson.Gson;
+import lombok.Getter;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.iam.model.CreateAccessKeyRequest;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+import software.amazon.awssdk.services.securitylake.model.AwsIdentity;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -35,8 +43,6 @@ public class RDSFetch {
 
         String secretName = "rds_login";
         String region = "us-east-1";
-        //String accessKey = getAccess();
-        //String secretKey = getSecret();
 
         // Create a Secrets Manager client
         SecretsManagerClient client = SecretsManagerClient.builder()
@@ -47,12 +53,12 @@ public class RDSFetch {
                         AwsCredentials awsCredentials = new AwsCredentials() {
                             @Override
                             public String accessKeyId() {
-                                return "";
+                                return System.getenv("AWS_ACCESS_KEY_ID");
                             }
 
                             @Override
                             public String secretAccessKey() {
-                                return "";
+                                return System.getenv("AWS_SECRET_ACCESS_KEY");
                             }
                         };
 
@@ -76,54 +82,4 @@ public class RDSFetch {
         String secret = getSecretValueResponse.secretString();
         return gson.fromJson(secret, RDSLogin.class);
     }
-
-//    private String getAccess() {
-//
-//        String secretName = "security_credentials";
-//        Region region = Region.of("us-east-1");
-//
-//        // Create a Secrets Manager client
-//        SecretsManagerClient client = SecretsManagerClient.builder()
-//                .region(region)
-//                .build();
-//
-//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-//                .secretId(secretName)
-//                .build();
-//
-//        GetSecretValueResponse getSecretValueResponse;
-//
-//        try {
-//            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
-//        } catch (Exception e) {
-//            throw e;
-//        }
-//
-//        return getSecretValueResponse.secretString();
-//    }
-//
-//    private String getSecret() {
-//
-//        String secretName = "security_access_secret";
-//        Region region = Region.of("us-east-1");
-//
-//        // Create a Secrets Manager client
-//        SecretsManagerClient client = SecretsManagerClient.builder()
-//                .region(region)
-//                .build();
-//
-//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-//                .secretId(secretName)
-//                .build();
-//
-//        GetSecretValueResponse getSecretValueResponse;
-//
-//        try {
-//            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
-//        } catch (Exception e) {
-//            throw e;
-//        }
-//
-//        return getSecretValueResponse.secretString();
-//    }
 }
