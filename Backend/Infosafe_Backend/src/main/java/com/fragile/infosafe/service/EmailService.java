b8a246b089180f7b777fd9f0443b55d4c96cdfc1
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2023-09-06 11:25:38 +0200

    Implementing notification

---
+++ Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/service/EmailService.java
@@ -0,0 +1,28 @@
+package com.fragile.infosafe.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendPasswordEmail(String to, String password) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject("Your New Password");
+        helper.setText("Your new password is: " + password);
+
+        emailSender.send(message);
+    }
+}
+
