111a25e11d64e644562613db0024100efe1a0f3b
Christof Steyn <56771007+P3TROOS@users.noreply.github.com>
2023-09-06 11:23:20 +0200

    Update DatabaseConfig.java

--- Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/config/DatabaseConfig.java
+++ Backend/Infosafe_Backend/src/main/java/com/fragile/infosafe/config/DatabaseConfig.java
@@ -1,35 +1,103 @@
 package com.fragile.infosafe.config;
 
-import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
 
+    @Primary
     @Bean(name = "primaryDataSource")
-    @Qualifier("primaryDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource primaryDataSource() {
-        return DataSourceBuilder
-                .create()
-                .url("jdbc:mysql://localhost:3306/infosafe")
-                .username("root")
-                .password("")
-                .driverClassName("com.mysql.cj.jdbc.Driver")
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @ConfigurationProperties(prefix = "spring.secondary-datasource")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("primaryDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.fragile.infosafe.model")
+                .persistenceUnit("primary")
                 .build();
     }
 
-    @Bean(name = "secondDataSource")
-    @Qualifier("secondDataSource")
-    public DataSource secondDataSource() {
-        return DataSourceBuilder
-                .create()
-                .url("jdbc:mysql://localhost:3306/backup")
-                .username("root")
-                .password("")
-                .driverClassName("com.mysql.cj.jdbc.Driver")
+    @Bean(name = "secondaryEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean secondaryEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("secondaryDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.fragile.infosafe.model")
+                .persistenceUnit("secondary")
                 .build();
     }
+
+    @Primary
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager((jakarta.persistence.EntityManagerFactory) entityManagerFactory);
+    }
+
+    @Bean(name = "secondaryTransactionManager")
+    public PlatformTransactionManager secondaryTransactionManager(
+            @Qualifier("secondaryEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager((jakarta.persistence.EntityManagerFactory) entityManagerFactory);
+    }
 }
+
+
+//import javax.sql.DataSource;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DatabaseConfig {
+//
+//    @Bean(name = "primaryDataSource")
+//    @Qualifier("primaryDataSource")
+//    public DataSource primaryDataSource() {
+//        return DataSourceBuilder
+//                .create()
+//                .url("jdbc:mysql://localhost:3306/infosafe")
+//                .username("root")
+//                .password("")
+//                .driverClassName("com.mysql.cj.jdbc.Driver")
+//                .build();
+//    }
+//
+//    @Bean(name = "secondDataSource")
+//    @Qualifier("secondDataSource")
+//    public DataSource secondDataSource() {
+//        return DataSourceBuilder
+//                .create()
+//                .url("jdbc:mysql://localhost:3306/backup")
+//                .username("root")
+//                .password("")
+//                .driverClassName("com.mysql.cj.jdbc.Driver")
+//                .build();
+//    }
+//}
