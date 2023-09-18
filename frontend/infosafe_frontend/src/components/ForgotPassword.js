a2d20a17bebbe359e141a4e5c66a0a29bbe09a0e
Karel-Louis Smit <u20447932@tuks.co.za>
2023-09-18 02:42:48 +0200

    Added forgot password styling

--- frontend/infosafe_frontend/src/components/ForgotPassword.js
+++ frontend/infosafe_frontend/src/components/ForgotPassword.js
@@ -4,35 +4,40 @@ export const ForgotPassword = () => {
 
     const emailState = () => {
         document.getElementById("emailPanel").style.display = "none";
-        document.getElementById("otpPanel").style.display = "inline";
+        document.getElementById("otpPanel").style.display = "block";
     }
     const otpState = () => {
         document.getElementById("otpPanel").style.display = "none";
-        document.getElementById("passwordPanel").style.display = "inline";
+        document.getElementById("passwordPanel").style.display = "block";
     }
 
     return (
         <div className="background">
             <div className="inputPanel">
+                <div className="title">
+                    <p className="forgotTitle">
+                        FORGOT PASSWORD
+                    </p>
+                </div>
             <div className="emailPanel" id="emailPanel">
-                <p>Enter your E-Mail:</p>
-                <input type="text"/>
+                <p className="emailTitle">Enter your E-Mail:</p>
+                <input className="forgotEmail" type="text"/>
                 <button className="submit" onClick={emailState}>
                     Submit
                 </button>
             </div>
             <div className="otpPanel" id="otpPanel">
-                <p>Enter OTP:</p>
-                <input type="text"/>
+                <p className="otpTitle">Enter OTP:</p>
+                <input className="forgotOTP" type="text"/>
                 <button className="submit" onClick={otpState}>
                     Submit
                 </button>
             </div>
             <div className="passwordPanel" id="passwordPanel">
-                <p>Enter new password:</p>
-                <input type="text"/>
-                <p>Re-enter new password:</p>
-                <input type="text"/>
+                <p className="passTitle">Enter new password:</p>
+                <input className="forgotPass" type="text"/>
+                <p className="rePassTitle">Re-enter new password:</p>
+                <input className="forgotRePass" type="text"/>
                 <button className="submit">
                     Submit
                 </button>
