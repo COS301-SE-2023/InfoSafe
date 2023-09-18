863c0990957d1161b594b17985cc352d6db42aef
Chris Mittendorf <u15092462@tuks.co.za>
2023-09-18 21:25:22 +0200

    RDS fetch deleted

--- backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/config/RDSFetch.java
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/config/RDSFetch.java
@@ -1,147 +1,176 @@
-package com.fragile.infosafe.config;
-
-import com.google.gson.Gson;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
-import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class RDSFetch {
-
-    private Gson gson = new Gson();
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(datasource());
-        em.setPackagesToScan("com.fragile.infosafe");
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
-        vendorAdapter.setShowSql(true);
-
-        em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaPropertyMap(jpaProperties());
-        return em;
-    }
-
-    @Bean
-    public DataSource datasource() {
-
-        RDSLogin login = getRDSLogin();
-
-        return DataSourceBuilder
-                .create()
-                .driverClassName("com.mysql.cj.jdbc.Driver")
-                .url("jdbc:" + login.getEngine() + "://" + login.getHost() + ":" + login.getPort() + "/" + login.getDbname())
-                .username(login.getUsername())
-                .password(login.getPassword())
-                .build();
-    }
-
-    @Bean
-    public Map<String, Object> jpaProperties() {
-        Map<String, Object> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "update");
-        return properties;
-    }
-
-    private RDSLogin getRDSLogin() {
-
-        String secretName = "rds_login";
-        String region = "us-east-1";
-
-        // Create a Secrets Manager client
-        SecretsManagerClient client = SecretsManagerClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(new AwsCredentialsProvider() {
-                    @Override
-                    public AwsCredentials resolveCredentials() {
-                        AwsCredentials awsCredentials = new AwsCredentials() {
-                            @Override
-                            public String accessKeyId() {
-                                return System.getenv("AWS_ACCESS_KEY_ID");
-                            }
-
-                            @Override
-                            public String secretAccessKey() {
-                                return System.getenv("AWS_SECRET_ACCESS_KEY");
-                            }
-                        };
-
-                        return awsCredentials;
-                    }
-                })
-                .build();
-
-        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-                .secretId(secretName)
-                .build();
-
-        GetSecretValueResponse getSecretValueResponse;
-
-        try {
-            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
-        } catch (Exception e) {
-            throw e;
-        }
-
-        String secret = getSecretValueResponse.secretString();
-        return gson.fromJson(secret, RDSLogin.class);
-    }
-
-    private DeleteDBLogin getDeleteDBLogin() {
-
-        String secretName = "delete_login";
-        String region = "us-east-1";
-
-        // Create a Secrets Manager client
-        SecretsManagerClient client = SecretsManagerClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(new AwsCredentialsProvider() {
-                    @Override
-                    public AwsCredentials resolveCredentials() {
-                        AwsCredentials awsCredentials = new AwsCredentials() {
-                            @Override
-                            public String accessKeyId() {
-                                return System.getenv("AWS_ACCESS_KEY_ID");
-                            }
-
-                            @Override
-                            public String secretAccessKey() {
-                                return System.getenv("AWS_SECRET_ACCESS_KEY");
-                            }
-                        };
-
-                        return awsCredentials;
-                    }
-                })
-                .build();
-
-        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
-                .secretId(secretName)
-                .build();
-
-        GetSecretValueResponse getSecretValueResponse;
-
-        try {
-            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
-        } catch (Exception e) {
-            throw e;
-        }
-
-        String secret = getSecretValueResponse.secretString();
-        return gson.fromJson(secret, DeleteDBLogin.class);
-    }
-}
+//package com.fragile.infosafe.primary.config;
+//
+//import com.google.gson.Gson;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+//import software.amazon.awssdk.auth.credentials.AwsCredentials;
+//import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+//import software.amazon.awssdk.regions.Region;
+//import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+//import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+//import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+//import javax.sql.DataSource;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class RDSFetch {
+//
+//    private Gson gson = new Gson();
+//
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//        LocalContainerEntityManagerFactoryBean emP = new LocalContainerEntityManagerFactoryBean();
+//        emP.setDataSource(datasourcePrimary());
+//        emP.setPackagesToScan("com.fragile.infosafe.primary");
+//
+//        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+//        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+//        vendorAdapter.setShowSql(true);
+//
+//        emP.setJpaVendorAdapter(vendorAdapter);
+//        emP.setJpaPropertyMap(jpaProperties());
+//        return emP;
+//    }
+//
+////    @Bean
+////    public LocalContainerEntityManagerFactoryBean entityManagerFactorySecondary() {
+////        LocalContainerEntityManagerFactoryBean emP = new LocalContainerEntityManagerFactoryBean();
+////        emP.setDataSource(datasourceSecondary());
+////        emP.setPackagesToScan("com.fragile.infosafe.delete");
+////
+////        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+////        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+////        vendorAdapter.setShowSql(true);
+////
+////        emP.setJpaVendorAdapter(vendorAdapter);
+////        emP.setJpaPropertyMap(jpaProperties());
+////        return emP;
+////    }
+//
+//    @Bean
+//    public DataSource datasourcePrimary() {
+//
+//        RDSLogin login = getRDSLogin();
+//
+//        return DataSourceBuilder
+//                .create()
+//                .driverClassName("com.mysql.cj.jdbc.Driver")
+//                .url("jdbc:" + login.getEngine() + "://" + login.getHost() + ":" + login.getPort() + "/" + login.getDbname())
+//                .username(login.getUsername())
+//                .password(login.getPassword())
+//                .build();
+//    }
+//
+//    @Bean
+//    public DataSource datasourceSecondary() {
+//
+//        DeleteDBLogin login = getDeleteDBLogin();
+//
+//        return DataSourceBuilder
+//                .create()
+//                .driverClassName("com.mysql.cj.jdbc.Driver")
+//                .url("jdbc:" + login.getEngine() + "://" + login.getHost() + ":" + login.getPort() + "/" + login.getDbname())
+//                .username(login.getUsername())
+//                .password(login.getPassword())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Map<String, Object> jpaProperties() {
+//        Map<String, Object> properties = new HashMap<>();
+//        properties.put("hibernate.hbm2ddl.auto", "update");
+//        return properties;
+//    }
+//
+//    private RDSLogin getRDSLogin() {
+//
+//        String secretName = "rds_login";
+//        String region = "us-east-1";
+//
+//        // Create a Secrets Manager client
+//        SecretsManagerClient client = SecretsManagerClient.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(new AwsCredentialsProvider() {
+//                    @Override
+//                    public AwsCredentials resolveCredentials() {
+//                        AwsCredentials awsCredentials = new AwsCredentials() {
+//                            @Override
+//                            public String accessKeyId() {
+//                                return System.getenv("AWS_ACCESS_KEY_ID");
+//                            }
+//
+//                            @Override
+//                            public String secretAccessKey() {
+//                                return System.getenv("AWS_SECRET_ACCESS_KEY");
+//                            }
+//                        };
+//
+//                        return awsCredentials;
+//                    }
+//                })
+//                .build();
+//
+//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+//                .secretId(secretName)
+//                .build();
+//
+//        GetSecretValueResponse getSecretValueResponse;
+//
+//        try {
+//            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+//        } catch (Exception e) {
+//            throw e;
+//        }
+//
+//        String secret = getSecretValueResponse.secretString();
+//        return gson.fromJson(secret, RDSLogin.class);
+//    }
+//
+//    private DeleteDBLogin getDeleteDBLogin() {
+//
+//        String secretName = "delete_login";
+//        String region = "us-east-1";
+//
+//        // Create a Secrets Manager client
+//        SecretsManagerClient client = SecretsManagerClient.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(new AwsCredentialsProvider() {
+//                    @Override
+//                    public AwsCredentials resolveCredentials() {
+//                        AwsCredentials awsCredentials = new AwsCredentials() {
+//                            @Override
+//                            public String accessKeyId() {
+//                                return System.getenv("AWS_ACCESS_KEY_ID");
+//                            }
+//
+//                            @Override
+//                            public String secretAccessKey() {
+//                                return System.getenv("AWS_SECRET_ACCESS_KEY");
+//                            }
+//                        };
+//
+//                        return awsCredentials;
+//                    }
+//                })
+//                .build();
+//
+//        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+//                .secretId(secretName)
+//                .build();
+//
+//        GetSecretValueResponse getSecretValueResponse;
+//
+//        try {
+//            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+//        } catch (Exception e) {
+//            throw e;
+//        }
+//
+//        String secret = getSecretValueResponse.secretString();
+//        return gson.fromJson(secret, DeleteDBLogin.class);
+//    }
+//}
