58e3108554efeeab1c173d75b43393429d444693
Chris Mittendorf <u15092462@tuks.co.za>
2023-09-18 22:08:08 +0200

    Update Token.java

--- backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/token/Token.java
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/token/Token.java
@@ -1,15 +1,7 @@
 package com.fragile.infosafe.primary.token;
 
 import com.fragile.infosafe.primary.model.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name="tokens")
 public class Token {
 
     @Id
