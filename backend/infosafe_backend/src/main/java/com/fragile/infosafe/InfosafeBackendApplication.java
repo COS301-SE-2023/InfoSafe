6d957b4ab9c3d7094c1b044569d919b8ed46c125
Chris Mittendorf <u15092462@tuks.co.za>
2023-09-18 10:33:35 +0200

    Removed unused imports

--- backend/infosafe_backend/src/main/java/com/fragile/infosafe/InfosafeBackendApplication.java
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/InfosafeBackendApplication.java
@@ -1,13 +1,8 @@
 package com.fragile.infosafe;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 import java.util.Collections;
-import java.util.Date;
-
-import static com.fragile.infosafe.model.Role.*;
-
 
 @SpringBootApplication
 public class InfosafeBackendApplication {
