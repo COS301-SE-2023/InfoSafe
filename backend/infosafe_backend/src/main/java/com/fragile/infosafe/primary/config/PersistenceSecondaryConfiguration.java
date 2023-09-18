086b50b59ab9356cc823beb0e95f880fba410a70
Chris Mittendorf <u15092462@tuks.co.za>
2023-09-18 21:36:32 +0200

    Secondary database updated
    Secondarydatabase updated with aws authentication and login

--- backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/config/PersistenceSecondaryConfiguration.java
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/config/PersistenceSecondaryConfiguration.java
@@ -1,33 +1,37 @@
 package com.fragile.infosafe.primary.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import com.google.gson.Gson;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import javax.sql.DataSource;
 import java.util.HashMap;
-import java.util.Objects;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
-// @PropertySource({ "classpath:application.properties" })
 @EnableJpaRepositories(
         basePackages = {"com.fragile.infosafe.delete"},
         entityManagerFactoryRef = "secondaryEntityManager",
         transactionManagerRef = "secondaryTransactionManager"
 )
 public class PersistenceSecondaryConfiguration {
-    private final Environment env;
+
+    private Gson gson = new Gson();
+
     @Bean
     public LocalContainerEntityManagerFactoryBean secondaryEntityManager() {
         final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
@@ -36,6 +40,7 @@ public class PersistenceSecondaryConfiguration {
 
         final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
+        vendorAdapter.setShowSql(true);
         final HashMap<String, Object> properties = new HashMap<String, Object>();
         properties.put("hibernate.hbm2ddl.auto", "update");
         properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
@@ -43,12 +48,63 @@ public class PersistenceSecondaryConfiguration {
 
         return em;
     }
+
     @Bean
     @ConfigurationProperties(prefix="spring.datasource.second")
     public DataSource secondaryDataSource() {
-        return DataSourceBuilder.create().build();
+        DeleteDBLogin login = getDeleteDBLogin();
+        return DataSourceBuilder
+                .create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:" + login.getEngine() + "://" + login.getHost() + ":" + login.getPort() + "/secondary_database") //+ login.getDbname())
+                .username(login.getUsername())
+                .password(login.getPassword())
+                .build();
     }
 
+    private DeleteDBLogin getDeleteDBLogin() {
+
+        String secretName = "rds_login";
+        String region = "us-east-1";
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(new AwsCredentialsProvider() {
+                    @Override
+                    public AwsCredentials resolveCredentials() {
+                        AwsCredentials awsCredentials = new AwsCredentials() {
+                            @Override
+                            public String accessKeyId() {
+                                return System.getenv("AWS_ACCESS_KEY_ID");
+                            }
+
+                            @Override
+                            public String secretAccessKey() {
+                                return System.getenv("AWS_SECRET_ACCESS_KEY");
+                            }
+                        };
+
+                        return awsCredentials;
+                    }
+                })
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        String secret = getSecretValueResponse.secretString();
+        return gson.fromJson(secret, DeleteDBLogin.class);
+    }
 
     @Bean
     public PlatformTransactionManager secondaryTransactionManager() {
@@ -57,4 +113,3 @@ public class PersistenceSecondaryConfiguration {
         return transactionManager;
     }
 }
-
