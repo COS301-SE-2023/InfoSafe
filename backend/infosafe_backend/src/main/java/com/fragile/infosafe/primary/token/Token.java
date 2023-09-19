4fd2479f7763a188ecc2cc745d9fb6869422c5e4
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2023-09-19 21:49:36 +0200

    Trying to fix cors errors

--- backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/token/Token.java
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/token/Token.java
@@ -7,29 +7,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name="tokens")
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//@Table(name="tokens")
 public class Token {
-
-    @Id
-    @GeneratedValue
-    public Integer id;
-
-    @Column(unique = true)
-    public String token;
-
-    @Enumerated(EnumType.STRING)
-    public TokenType tokenType = TokenType.BEARER;
-
-    public boolean revoked;
-
-    public boolean expired;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    public User user;
+//
+//    @Id
+//    @GeneratedValue
+//    public Integer id;
+//
+//    @Column(unique = true)
+//    public String token;
+//
+//    @Enumerated(EnumType.STRING)
+//    public TokenType tokenType = TokenType.BEARER;
+//
+//    public boolean revoked;
+//
+//    public boolean expired;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    public User user;
 }
