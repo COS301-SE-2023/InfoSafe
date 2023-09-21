105719587faa76a61369ff91c5f5cdcd8f1f31b1
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-09-22 00:10:04 +0200

    Updated View User design and styling.

--- frontend/infosafe_frontend/src/components/View/ViewUser.js
+++ frontend/infosafe_frontend/src/components/View/ViewUser.js
@@ -12,28 +12,33 @@ const ViewUser = ({ user, popupClose, popupOpen }) => {
             position="center center"
         >
             <div className="viewUserOverlay">
-                <div className="borderView">
-                    <button className="viewUserBackButton" onClick={popupClose} data-testid="back-button">
-                        <IoArrowBackOutline className="viewUserBackIcon" />
-                    </button>
-                    <p className="viewUserTitle">View User</p>
-                    <div className="view_name">
-                        <p className="nameDisplay">Name</p>
-                        <p className="viewName">{user.first_name}</p>
-                    </div>
-                    <div className="view_surname">
-                        <p className="surnameDisplay">Surname</p>
-                        <p className="viewSurname">{user.last_name}</p>
-                    </div>
-                    <div className="view_email">
-                        <p className="emailDisplay">Email</p>
-                        <p className="viewEmail">{user.email}</p>
-                    </div>
-                    <div className="view_role">
-                        <p className="roleDisplay">System Role</p>
-                        <p className="viewRole">{user.role.role_name}</p>
+                <div className="popupBackground">
+                    <div className="borderView">
+                        <button className="viewUserBackButton" onClick={popupClose} data-testid="back-button">
+                            <IoArrowBackOutline className="viewUserBackIcon" />
+                        </button>
+                        <p className="viewUserTitle">View User</p>
+                        <div className="viewUserContent">
+                            <div className="view_name">
+                                <p className="nameDisplay">Name</p>
+                                <p className="viewName">{user.first_name}</p>
+                            </div>
+                            <div className="view_surname">
+                                <p className="surnameDisplay">Surname</p>
+                                <p className="viewSurname">{user.last_name}</p>
+                            </div>
+                            <div className="view_email">
+                                <p className="emailDisplay">Email</p>
+                                <p className="viewEmail">{user.email}</p>
+                            </div>
+                            <div className="view_role">
+                                <p className="viewUserRoleDisplay">System Role</p>
+                                <p className="viewRole">{user.role.role_name}</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </Popup>
     );
