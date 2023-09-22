dd754fe66714d9f37a3e634471677769379478b6
Karel-Louis Smit <u20447932@tuks.co.za>
2023-09-23 01:36:31 +0200

    Updated view task styling

--- frontend/infosafe_frontend/src/components/View/ViewTaskPopup.js
+++ frontend/infosafe_frontend/src/components/View/ViewTaskPopup.js
@@ -9,20 +9,24 @@ export const ViewTask = ({ task, popupClose, popupOpen }) => {
     return (
         <Popup open={popupOpen} closeOnDocumentClick={false}>
             <div className="viewTaskOverlay">
+                <div className="popupBackground">
                 <div className="borderViewTask">
-                    <button className="backButton" onClick={popupClose} data-testid="back-button">
-                        <IoArrowBackOutline className="backIcon" />
+                    <button className="viewTaskBackButton" onClick={popupClose} data-testid="back-button">
+                        <IoArrowBackOutline className="viewTaskBackIcon" />
                     </button>
-                    <p className="pageTitle">View Task</p>
-                    <p className="displayTitle">Task ID</p>
-                    <p className="displayData">Task {task.task_id}</p>
-                    <p className="displayTitle">Data Scope</p>
-                    <p className="displayData">Data Scope A</p>
-                    <p className="displayTitle">Task Description</p>
-                    <textarea className="viewTextArea" readOnly={true} value={task.task_description}/>
-                    <p className="displayTitle">Completion Date</p>
-                    <p className="displayData">{task.due_date}</p>
+                    <p className="viewTaskPageTitle">View Task</p>
+                    <div className="viewTaskContent">
+                    <p className="viewTaskDisplayTitle">Task ID</p>
+                    <p className="viewTaskDisplayData">Task {task.task_id}</p>
+                    <p className="viewTaskDisplayTitle">Data Scope</p>
+                    <p className="viewTaskDisplayData">Data Scope A</p>
+                    <p className="viewTaskDisplayTitle">Task Description</p>
+                    <textarea className="viewTaskTextArea" readOnly={true} value={task.task_description}/>
+                    <p className="viewTaskDisplayTitle">Completion Date</p>
+                    <p className="viewTaskDisplayData">{task.due_date}</p>
                 </div>
+                </div>
+            </div>
             </div>
         </Popup>
     );
