da46c73255ccee0098ce7effcdb773e4b8f54b2d
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-09-26 23:37:37 +0200

    Added Help Pop-Up for Risks, needs content.

--- frontend/infosafe_frontend/src/components/Subsystems/Risks.js
+++ frontend/infosafe_frontend/src/components/Subsystems/Risks.js
@@ -8,6 +8,8 @@ import "../../styling/Risks.css";
 import {useGetPerms} from "../getData/getPerms";
 import {useGetRisk} from "../getData/getRisk";
 import {RiEditBoxFill} from "react-icons/ri";
+import {HelpPopup} from "../HelpPopup";
+import {IoHelpCircle} from 'react-icons/io5';
 
 export const Risks = () => {
     const [createRiskOpen, setCreateRiskOpen] = useState(false);
@@ -108,10 +110,23 @@ export const Risks = () => {
     showRisk.map((risk) =>
         risks.push(<ViewRisks risk={risk} key={risk.risk_id}/>)
     )
+    const [helpOpen, setHelpOpen] = useState(false);
+
+    const helpMsg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     return(
         <div className="display">
             <div className="risksBackground">
+                <button  className="risksHelpButton" onClick={() => setHelpOpen(true)}>
+                    <IoHelpCircle className="helpPopupIcon"></IoHelpCircle>
+                    {helpOpen ? (
+                        <HelpPopup
+                            popupClose={() => setHelpOpen(false)}
+                            popupOpen={helpOpen}
+                            message={helpMsg}
+                        />
+                    ) : null}
+                </button>
                 <div className="searchRisks">
                     <input
                         // data-testid="riskSearch"
