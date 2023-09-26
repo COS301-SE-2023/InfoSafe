e43e73942f176a0fa152bd7f9f611b99496a0b5a
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2023-09-26 23:34:32 +0200

    Added Help Pop-Up for Asset Requests, needs content.

--- frontend/infosafe_frontend/src/components/Subsystems/AssetRequest.js
+++ frontend/infosafe_frontend/src/components/Subsystems/AssetRequest.js
@@ -5,6 +5,8 @@ import "../../styling/AssetRequests.css";
 import {FaSearch} from "react-icons/fa";
 import {useGetAssAR} from "../getData/getAssAR";
 import {useGetPerms} from "../getData/getPerms";
+import {HelpPopup} from "../HelpPopup";
+import {IoHelpCircle} from 'react-icons/io5';
 export const AssetRequest = () => {
     const {roles} = useGetPerms();
     const {showAssetRequests} = useGetAssAR();
@@ -60,9 +62,24 @@ export const AssetRequest = () => {
     showAssetRequests.map((assetRequest) =>
         assetRequests.push(<ViewAssetRequests assetRequest={assetRequest} key={assetRequest.asset_request_id}/>)
     );
+
+    const [helpOpen, setHelpOpen] = useState(false);
+
+    const helpMsg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     return (
         <div className="display">
             <div className="assetRequestsBackground">
+                <button  className="assetRequestsHelpButton" onClick={() => setHelpOpen(true)}>
+                    <IoHelpCircle className="helpPopupIcon"></IoHelpCircle>
+                    {helpOpen ? (
+                        <HelpPopup
+                            popupClose={() => setHelpOpen(false)}
+                            popupOpen={helpOpen}
+                            message={helpMsg}
+                        />
+                    ) : null}
+                </button>
                 <div className="searchAssetRequests">
                     <input
                         // data-testid="assetRequestSearch"
