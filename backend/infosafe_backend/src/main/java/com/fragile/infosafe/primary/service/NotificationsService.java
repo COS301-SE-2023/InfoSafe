3c0010be097640963a7ea46a21fbc49b4ce54134
Alistair Ross <87852242+AlistairMRoss@users.noreply.github.com>
2023-09-28 01:29:36 +0200

    Added notifications because the merge destroyed them

--- backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/service/NotificationsService.java
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/service/NotificationsService.java
@@ -1,4 +1,45 @@
 package com.fragile.infosafe.primary.service;
 
+import com.fragile.infosafe.primary.model.Notifications;
+import com.fragile.infosafe.primary.model.User;
+import com.fragile.infosafe.primary.repository.NotificationsRepository;
+import com.fragile.infosafe.primary.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class NotificationsService {
+
+    private final NotificationsRepository notificationsRepository;
+    private final UserRepository userRepository;
+
+    @Scheduled(cron = "0 0 */10 * * *")
+    public void deleteOldRecords() {
+        LocalDateTime twelveHoursAgo = LocalDateTime.now().minusHours(12);
+        notificationsRepository.deleteByCreatedAtBefore(twelveHoursAgo);
+    }
+
+    public void makeNotification(String message, User user){
+        String timeMade = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        Notifications notification = Notifications.builder()
+                .createdAt(LocalDateTime.now())
+                .notification(message)
+                .user(user)
+                .timeMade(timeMade)
+                .build();
+        notificationsRepository.save(notification);
+    }
+
+    public List<Notifications> getNotifications(User user) {
+        log.info(String.valueOf(user));
+        return notificationsRepository.findByUserUserId(user.getUser_id());
+    }
 }
