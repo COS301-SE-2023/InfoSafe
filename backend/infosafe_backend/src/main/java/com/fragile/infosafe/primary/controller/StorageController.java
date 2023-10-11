cb90e2f2bc34aeb0fcc52b2d94e23ab9cb6b75a7
Chris Mittendorf <u15092462@tuks.co.za>
2023-10-11 15:06:02 +0200

    Storage Controller

---
+++ backend/infosafe_backend/src/main/java/com/fragile/infosafe/primary/controller/StorageController.java
@@ -0,0 +1,40 @@
+package com.fragile.infosafe.primary.controller;
+
+import com.fragile.infosafe.primary.service.StorageService;
+import com.sun.mail.iap.ByteArray;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@Slf4j
+@RequestMapping("api/storage")
+public class StorageController {
+
+    private StorageService service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file){
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName){
+        byte[] data = service.dowloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName){
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
