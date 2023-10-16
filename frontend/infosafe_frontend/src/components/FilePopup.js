0a053c19bb550171c663fd039c89c8a4b55ca327
Karel-Louis Smit <u20447932@tuks.co.za>
2023-10-16 19:30:51 +0200

    Updated files styling

--- frontend/infosafe_frontend/src/components/FilePopup.js
+++ frontend/infosafe_frontend/src/components/FilePopup.js
@@ -15,25 +15,26 @@ export const FilePopup = ({popupOpen,popupClose,datascope}) =>{
                             <IoArrowBackOutline className="backIcon"/>
                         </button>
                         <p className="filePageTitle">Files</p>
-
-                        <p className="fileLabel">Choose File</p>
-                        <Dropdown
-                            options={FILES}
-                            value={FILES[0]}
-                            className="fileDropdown"
-                            name="createRiskProbabilityDropdown"
-                            //onChange={}
-                        />
-                        <div className="fileButtonsDiv">
-                            <button className="fileDownload">
-                                Download
-                            </button>
-                            <button className="fileDelete">
-                                Delete
-                            </button>
+                        <div className="fileContent">
+                            <p className="fileLabel">Choose File</p>
+                            <Dropdown
+                                options={FILES}
+                                value={FILES[0]}
+                                className="fileDropdown"
+                                name="createRiskProbabilityDropdown"
+                                //onChange={}
+                            />
+                            <div className="fileButtonsDiv">
+                                <button className="fileDownload">
+                                    Download
+                                </button>
+                                <button className="fileDelete">
+                                    Delete
+                                </button>
+                            </div>
+                            <p className="uploadLabel">Upload File</p>
+                            <input type="file" className="fileUpload"/>
                         </div>
-                        <p className="uploadLabel">Upload File</p>
-                        <input type="file" className="fileUpload"/>
                     </div>
                 </div>
             </div>
